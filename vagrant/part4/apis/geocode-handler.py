96bebe9b38f2f36ac59f819231c4f8c7e3209332
alessandrobisiani <bisi90@gmail.com>
2019-03-25 16:34:40 -0400

    feat: geocode handler returns a tuple: (lat, lon)

--- vagrant/part4/apis/geocode-handler.py
+++ vagrant/part4/apis/geocode-handler.py
@@ -14,3 +14,10 @@ def getGeocodeLocation(inputString):
             '''https://maps.googleapis.com/maps/api/geocode/json
             ?address={}&key={}'''.format(location_string, google_api_key)
             )
+    h = httplib2.Http()
+    response, content = h.request(url, 'GET')
+    result = json.loads(content)
+
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude, longitude)
